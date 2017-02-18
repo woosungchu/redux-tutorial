019c0f51fc8a71c4b63957c95fcf062d394f404f
woosungchu <woosungchu@gmail.com>
2017-02-18 23:10:17 +0900

    basic JSX

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+//CamelCase
 class App extends React.Component {
+  sayHey(){
+    alert('Hey!');
+  }
   render(){
+    let text = "Dev-Server";
+    let pStyle = {
+      color : 'aqua',
+      backgroundColor : 'black'
+    }
     return (
-      <h1>Hello React Skeleton</h1>
+      <div>
+        <h1>Hello React Skeleton</h1>
+        <h2> Welcome to {text}</h2>
+        <button onClick={this.sayHey}>Click Me</button>
+        <p style = {pStyle}>{1 == 1 ? 'True':'False'}</p>
+        { /* comments */ }
+      </div>
     );
   }
 }
